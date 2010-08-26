396fdaffec23a14e32f06dfbbae18ff5f47cea35
yefe <yefe@316258ab-b9b8-444a-afaa-1c542d83d999>
2010-08-26 07:44:25 +0000

    git-svn-id: http://oa.0550.com:8080/svn/51yuedu/src@10 316258ab-b9b8-444a-afaa-1c542d83d999

--- website/utils/pinyin.py
+++ website/utils/pinyin.py
@@ -1,58 +1,53 @@
-#!/usr/bin/python
-# -*- coding: cp936 -*-
+# -*- coding: gbk -*-
 # $Id$
-# Copyright yefe<yefe@ichuzhou.cn>
+# Copyright 2010 Yefe<yefe@ichuzhou.cn>
 import os
 
 
-def stoi(s):
-    h = '0x'
-    for i in s: h += hex(ord(i))[-2:]
-    return int(h, 16)
-
-class Hanzi2Pinyin:
+class Hanzi2Pinyin(object):
     def __init__(self):
-        "Load data table"
+        self.table = {}
         try:
-            fp = open(os.path.normpath(os.path.join(os.path.dirname(__file__), 'pinyin.txt')))
+            fp = open(os.path.join(os.path.dirname(__file__), 'pinyin.txt'))
         except IOError:
             raise Exception("Can't load data from pinyin.txt\nPlease make sure this file exists.")
         else:
-            self.data = fp.read()
-            self.table = {}
+            self.data = fp.read().decode('gbk')
             for l in self.data.split('\n'):
-                self.table[l[0:2]] = l[2:]
+                self.table[l[0]] = l[1:]
             fp.close()
 
     def convert(self, value):
-        "Convert GB2312 to PinYin"
         pinyin = []
         tASCII = ''
         # 字符检查
-        for c in value:
-            if c == ' ':
-                if len(tASCII):
-                    pinyin.append(tASCII)
-                tASCII = ''
-            try:
-                i = stoi(c.encode('gb2312'))
-            except UnicodeEncodeError:
-                continue
-            # 48-57 (0-9)    65-90(A-Z)    97-122(a-z)    gb2312汉字部分
-            if (i>=48 and i<=57) or (i>=65 and i<=90) or (i>=97 and i<=122):
+        for c in value.lower() + ' ': # 加个空格多一次循环 修正尾部字符丢失问题
+            # 事实证明 ord 比 has_key 要节省性能， ord 放前面先做判断
+            i = ord(c)
+            if (i >= 48 and i <= 57) or (i >= 97 and i <= 122): # 48-57[0-9]   97-122[a-z]
                 tASCII += c
-            elif i>=0xb0a1 and i<=0xfffe and i not in (0xbfff,0xcfff,0xdfff,0xefff):
-                try:
-                    p = self.table[c.encode('gb2312')]
-                    pinyin.append(p)
-                except IndexError:
-                    pass
-        if tASCII != '':
-            pinyin.append(tASCII)
+                continue
+            
+            tASCII and pinyin.append(tASCII)
+            tASCII = ''
+            
+            if self.table.has_key(c):
+                pinyin.append(self.table[c])
+                continue
+        
         return pinyin
 
 
 if __name__ == '__main__':
-    print Hanzi2Pinyin().convert(u'你                   。，%……#￥#@@#￥Y^#@$#@%^%*&^&*               好12 345 Big Pig   ')
+    import time
+    t = u'Prep 你好    中 国！I Love China! 2010年8月 !@    #   $%^   &* ()_+   Append'
+    s = time.time()
+    p = Hanzi2Pinyin() # you class
+    print 'init:', time.time() - s
+    s = time.time()
+    print '-'.join(p.convert(t)) # you convert
+    for i in xrange(0,10000):
+        p.convert(t) # you convert
+    print 'convert:', time.time() - s
 
 
