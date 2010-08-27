21ca2446d985f6d2d44c4c8f38d1872ff2cab7cf
yefe <yefe@316258ab-b9b8-444a-afaa-1c542d83d999>
2010-08-27 02:39:40 +0000

    git-svn-id: http://oa.0550.com:8080/svn/51yuedu/src@13 316258ab-b9b8-444a-afaa-1c542d83d999

--- website/apps/book/models.py
+++ website/apps/book/models.py
@@ -66,7 +66,7 @@ class Book(models.Model):
     is_complete = models.BooleanField(u'已完本', default=False, db_index=True)
 
     search_title    = SphinxSearch(index='title')
-    search_author   = SphinxSearch(index='author')
+    search_author   = SphinxSearch(index='author_name')
 
     class Meta:
         #ordering = ('-id',)
